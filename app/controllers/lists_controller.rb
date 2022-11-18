76441a9d71c204ab64360435dcb6abbe6fdde52a
hugo chaumette <hugochaumette@gmail.com>
2022-11-18 14:54:04 +0100

    added dotenv-rails

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
   def index
     @lists = List.all
   end
@@ -17,12 +19,20 @@ class ListsController < ApplicationController
   end
 
   def show
-    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index new create show] do
+  root 'lists#index'
+  resources :lists, only: %i[index new create show destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
